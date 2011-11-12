eaea376af53dfc5c093d4e6db84a5226de1098b9
volodg <volodg@mail.ru>
2011-11-12 21:25:37 +0200

    first commit

---
+++ lib/dawg2dict/dawg2dict/Dawg2Dict.h
@@ -0,0 +1,13 @@
+//
+//  Dawg2Dict.h
+//  dawg2dict
+//
+//  Created by Vladimir on 12.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface Dawg2Dict : NSObject
+
+@end
---
+++ lib/dawg2dict/dawg2dict/Dawg2Dict.m
@@ -0,0 +1,13 @@
+//
+//  Dawg2Dict.m
+//  dawg2dict
+//
+//  Created by Vladimir on 12.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Dawg2Dict.h"
+
+@implementation Dawg2Dict
+
+@end
