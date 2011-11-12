fc7f2ed68f0e3f642f6b0f718101c63ed61d2302
volodg <volodg@mail.ru>
2011-11-12 21:28:26 +0200

    first commit

--- lib/dawg2dict/dawg2dict/Dawg2Dict.h
+++ lib/dawg2dict/dawg2dict/Dawg2Dict.h
@@ -1,13 +1,28 @@
-//
-//  Dawg2Dict.h
-//  dawg2dict
-//
-//  Created by Vladimir on 12.11.11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
+#ifndef DAWG2DICT_HEADER_H_INCLUDED
+#define DAWG2DICT_HEADER_H_INCLUDED
 
+#import <vector>
+#import <string>
 
+class DawgNode
+{
+public:
+   char letter;
+   bool last_in_word;
+   bool last_in_list;
+   int32_t next_node;
+public:
+   void unserialize( FILE* fp );
+};
 
-@interface Dawg2Dict : NSObject
+//GTODO fails on contains( "ZZZ" )
+class Dawg2Dict
+{
+   std::vector<DawgNode> nodes;
+   uint32_t next_node_for_char( uint32_t curr_node_index_, char letter, bool& last_in_word ) const;
+public:
+   bool load( const std::string& fname );
+   bool contains( const std::string& str ) const;
+};
 
-@end
+#endif //DAWG2DICT_HEADER_H_INCLUDED
