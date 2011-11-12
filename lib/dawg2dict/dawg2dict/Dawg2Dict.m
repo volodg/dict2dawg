387e5f0887d49f4f322fe0f867d670c053e0bbf7
volodg <volodg@mail.ru>
2011-11-12 22:08:36 +0200

    first commit

---
+++ lib/dawg2dict/dawg2dict/Dawg2Dict.m
@@ -0,0 +1,23 @@
+//
+//  Dawg2Dict.m
+//  Dawg2Dict
+//
+//  Created by Vladimir on 12.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Dawg2Dict.h"
+
+@implementation Dawg2Dict
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
